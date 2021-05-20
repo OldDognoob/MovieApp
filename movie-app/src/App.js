7d6dfca68e5b51a9d7be324593c4518614d35393
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-05-20 18:25:58 +0300

    wip

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,23 +1,28 @@
-import React,{useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import Movie from "./components/Movie";
 
-const FEATURED_API="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1"
-const IMG_API="https://image.tmdb.org/t/p/w1280"
-const SEARCH_API="https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
+const FEATURED_API =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 function App() {
   //called the api to get the movies
-   const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
   // any array of movies where we are going to loop over the movies
-  useEffect(()=>{
-    fetch(FEATURED_API).then(res => res.json())
-    .then(data =>{
-      console.log(data);
-      setMovies(data);
-    });
-  },[]);
+  useEffect(() => {
+    fetch(FEATURED_API)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMovies(data.results);
+      });
+  }, []);
 
-  return <div>{movies.length > 0 && movies.map((movie)=><Movie/>)}</div>
-
+  return <div>
+    {movies.length > 0 && movies.map((movie) => 
+    <Movie key={movie.id} data={movie}/>)}
+    </div>;
 }
 export default App;
--- movie-app/src/components/Movie.js
+++ movie-app/src/components/Movie.js
@@ -1,8 +1,10 @@
 import React from "react";
 
-const Movie=()=>(
-<div className="movie">
-        This is a movie component
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+
+const Movie=({title, poster_path, overview, vote_average})=>(
+    <div className="movie">
+        <img src={IMG_API + poster_path} alt={title}/>
     </div>
 );
 export default Movie;
