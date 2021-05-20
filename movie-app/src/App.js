11b32650ec5219690128537023afd2d458bda279
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-05-20 17:48:04 +0300

    wip-project

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
+import Movie from "./components/Movie";
 
-function App(){
-  return <div>Hello People</div>
+function App() {
+  // any array of movies where we are going to loop over the movies
+  const movies = ["1", "2", "3"];
+  return <div>
+   {movies.map(movie =>(
+   <Movie/>
+   ))}
+   
+    </div>;
 }
 export default App;
